a10ffcadb2f5d727effa17b8deadd565dd83bcbd
SantiagoDemattei <sdemattei@frba.utn.edu.ar>
2022-10-26 15:32:14 -0300

    landingpage anda, botones todavia no

--- src/main/java/controllers/IndexController.java
+++ src/main/java/controllers/IndexController.java
@@ -6,17 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     @RequestMapping("/")
-    String index(){
-        return "landingpage";
-    }
-
-    @RequestMapping("/iniciodesesion")
-    String iniciodesesion() {
-        return "iniciodesesion";
+    String landingpage(){
+        return "index";
     }
 
     @RequestMapping("/registro")
-     String registro() {
+    String registro(){
         return "registro";
     }
+
 }
--- src/main/java/controllers/UsuarioController.java
+++ src/main/java/controllers/UsuarioController.java
@@ -19,6 +19,8 @@ public class UsuarioController {
         this.uService = uService;
     }
 
+
+
     @GetMapping(path = "{userId}")
     public Usuario getUserById(@PathVariable("userId") Long uId) {
         return uService.obtenerUsuario(uId);
